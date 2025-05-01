db10e738d7447c28006ef6f6954ebd86a0021703
arthurlee <ykarthurlee@gmail.com>
2025-05-02 02:09:07 +0800

    Adds favicon and site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,13 @@ const inter = Inter({
 
 export const metadata = {
   generator: "v0.dev",
+  title: "Arthur's Portfolio",
+  description: "A portfolio for Arthur, a designer who loves to tinker and build.",
+  icons: {
+    icon: [ {url: '/favicon.ico'}, {url: '/favicon-16x16.png', sizes: '16x16'}, {url: '/favicon-32x32.png', sizes: '32x32'}],
+    apple: [ {url: '/apple-touch-icon.png', type: 'image/png'} ],
+    android: [ {url: '/android-chrome-192x192.png', sizes: '192x192'}, {url: '/android-chrome-512x512.png', sizes: '512x512'}],
+  },
 }
 
 export default function RootLayout({
